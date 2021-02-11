bd6288587e4454dda9e94a5dd35aae439a91a658
Filimon Raluca <raluca.filimon@info.uaic.ro>
2021-02-11 20:14:03 +0200

    eye selection and threshhold + more structure in code

--- eye_detection.py
+++ eye_detection.py
@@ -1,55 +1,86 @@
 import cv2
-import dlib
-import numpy
 import math
-from scipy.spatial import distance
+import numpy
+import frame_constants
 
-EYE_AR_THRESH = 0.2
-EYE_AR_CONSEC_FRAMES = 48
-WIDTH = 320
-HEIGHT = 240
-
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-
-blinks = 0
+prev_ratio = 0
 
 
-def euclidean_dist(x,y):
-    return math.sqrt(((x[0] - y[0]) ** 2) + ((x[1] - y[1]) ** 2))
+class EyeDetection:
+    def __init__(self, frame, drawing_frame, left_eye, right_eye):
+        self.frame = frame
+        self.drawing_frame = drawing_frame
+        self.left_eye = left_eye
+        self.right_eye = right_eye
+        self.ratio = None
 
-def eye_aspect_ratio(eye):
-    global blinks
-    a = euclidean_dist(eye[1], eye[5])
-    b =euclidean_dist(eye[2], eye[4])
-    c = euclidean_dist(eye[0], eye[3])
-    return (a + b) / (2.0 * c)
+    @staticmethod
+    def euclidian_dist(x, y):
+        return math.hypot((x[0] - y[0]), (x[1] - y[1]))
+
+    @staticmethod
+    def midpoint(x, y):
+        return int((x[0] + y[0]) / 2), int((x[1] + y[1]) / 2)
+
+    def draw_eye_line(self, eye):
+        cv2.line(self.drawing_frame, eye[0], eye[3], (0, 255, 0), 1)
+        center_top = self.midpoint(eye[1], eye[2])
+        center_buttom = self.midpoint(eye[4], eye[5])
+        cv2.line(self.drawing_frame, center_top, center_buttom, (0, 255, 0), 1)
+
+    def eye_aspect_ratio(self, eye):
+        a = self.euclidian_dist(eye[1], eye[5])
+        b = self.euclidian_dist(eye[2], eye[4])
+        c = self.euclidian_dist(eye[0], eye[3])
+        return (a + b) / (2.0 * c)
+
+    def detect_eye(self):
+        self.draw_eye_line(self.left_eye)
+        self.blinking()
+        self.gaze_detection()
+
+    def blinking(self):
+        global prev_ratio
+        self.ratio = (self.eye_aspect_ratio(self.left_eye) + self.eye_aspect_ratio(self.right_eye)) / 2.0
+        cv2.putText(self.drawing_frame, str(self.ratio), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+        if prev_ratio:
+            if prev_ratio - self.ratio > 0.03:
+                cv2.putText(self.drawing_frame, "blink", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
+        prev_ratio = self.ratio
+
+    def get_eye_region(self, eye):
+        eye_region = numpy.array(eye, numpy.int32)
+        min_x = numpy.min(eye_region[:, 0])
+        max_x = numpy.max(eye_region[:, 0])
+        min_y = numpy.min(eye_region[:, 1])
+        max_y = numpy.max(eye_region[:, 1])
+        return eye_region, (min_x, max_x, min_y, max_y)
+
+    @staticmethod
+    def select_eye(eye_rectangle, masked_frame):
+        eye_frame = masked_frame[eye_rectangle[2]:eye_rectangle[3],
+                    eye_rectangle[0]:eye_rectangle[1]]
+        eye_frame = cv2.resize(eye_frame, None, fx=10, fy=10)
+        _, threshhold_eye = cv2.threshold(eye_frame, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        return threshhold_eye
+
+    def gaze_detection(self):
+        left_eye_region, left_eye_rectangle = self.get_eye_region(self.left_eye)
+        right_eye_region, right_eye_rectangle = self.get_eye_region(self.right_eye)
+        mask = numpy.zeros((frame_constants.HEIGHT, frame_constants.WIDTH), numpy.uint8)
+        cv2.polylines(mask, [left_eye_region], True, (0, 0, 255), 1)
+        cv2.fillPoly(mask, [left_eye_region], 255)
+        cv2.polylines(mask, [right_eye_region], True, (0, 0, 255), 1)
+        cv2.fillPoly(mask, [right_eye_region], 255)
+        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        masked_frame = cv2.bitwise_and(gray, gray, mask=mask)
+        cv2.imshow("Eye masked", masked_frame)
+        left_eye_frame = self.select_eye(left_eye_rectangle, masked_frame)
+        right_eye_frame = self.select_eye(right_eye_rectangle, masked_frame)
+
+        cv2.imshow("left eye", left_eye_frame)
+        cv2.imshow("right eye", right_eye_frame)
 
 
-cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
-while True:
-    ret, frame = cam.read()
-    if ret is False:
-        break
-    frame = cv2.resize(frame, (WIDTH, HEIGHT))
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = detector(gray)
-    for face in faces:
-        landmarks = predictor(gray, face)
-        left_eye = []
-        right_eye = []
-        for x in range(0, 6):
-            left_eye.append((landmarks.part(36 + x).x, landmarks.part(36 + x).y))
-            right_eye.append((landmarks.part(42 + x).x, landmarks.part(42 + x).y))
-        if (eye_aspect_ratio(left_eye) + eye_aspect_ratio(right_eye)) / 2 < EYE_AR_THRESH:
-            blinks += 1
-            print("BLINK " + str(blinks))
-    cv2.imshow("Frame", gray)
-
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
-    if cv2.getWindowProperty('Frame', cv2.WND_PROP_VISIBLE) < 1:
-        break
-cam.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    pass
--- main.py
+++ main.py
@@ -1,74 +1,54 @@
 import cv2
 import dlib
 import math
-
-cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-WIDTH = 320
-HEIGHT = 240
-
-def euclidian_dist(x, y):
-    return math.hypot((x[0] - y[0]), (x[1] - y[1]))
+import numpy as np
+import frame_constants
+from eye_detection import EyeDetection
 
 
-def midpoint(x, y):
-    return int((x[0] + y[0]) / 2), int((x[1] + y[1]) / 2)
+class FaceRecognition:
+    def __init__(self):
+        self.cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+        self.landmarks = None
+        self.frame = None
+        self.drawing_frame = None
+        self.left_eye = None
+        self.right_eye = None
 
-
-def eye_aspect_ratio(eye):
-    a = euclidian_dist(eye[1], eye[5])
-    b = euclidian_dist(eye[2], eye[4])
-    c = euclidian_dist(eye[0], eye[3])
-    return (a + b) / (2.0 * c)
-
-
-def draw_eye_line(eye):
-    cv2.line(frame, eye[0], eye[3], (0, 255, 0), 1)
-    center_top = midpoint(eye[1], eye[2])
-    center_buttom = midpoint(eye[4], eye[5])
-    cv2.line(frame, center_top, center_buttom, (0, 255, 0), 1)
-
-prev_ratio = None
-
-while True:
-    ret, frame = cam.read()
-    if ret is False:
-        break
-    frame = cv2.resize(frame, (WIDTH, HEIGHT))
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = detector(gray)
-    for face in faces:
-        x = face.left()
-        y = face.top()
-        w = face.right() - x
-        h = face.bottom() - y
-
-        # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        landmarks = predictor(gray, face)
+    def draw_face_points(self):
         for i in range(0, 68):
-            x_p, y_p = landmarks.part(i).x, landmarks.part(i).y
-            cv2.circle(frame, (x_p, y_p), 2, (255, 0, 0), -1)
-        left_eye = []
-        right_eye = []
-        for x in range(0, 6):
-            left_eye.append((landmarks.part(36 + x).x, landmarks.part(36 + x).y))
-            right_eye.append((landmarks.part(42 + x).x, landmarks.part(42 + x).y))
-        draw_eye_line(left_eye)
-        draw_eye_line(right_eye)
-        ratio = (eye_aspect_ratio(left_eye) + eye_aspect_ratio(right_eye)) / 2.0
-        cv2.putText(frame, str(ratio), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
-        if prev_ratio:
-            if prev_ratio-ratio > 0.05:
-                cv2.putText(frame, "blink", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
-        prev_ratio = ratio
+            x_p, y_p = self.landmarks.part(i).x, self.landmarks.part(i).y
+            cv2.circle(self.drawing_frame, (x_p, y_p), 2, (255, 0, 0), -1)
 
-    cv2.imshow("Frame", frame)
+    def face_recognition(self):
+        while True:
+            ret, self.frame = self.cam.read()
+            if ret is False:
+                break
+            self.frame = cv2.resize(self.frame, (frame_constants.WIDTH, frame_constants.HEIGHT))
+            self.drawing_frame = self.frame.copy()
+            gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
 
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
-    if cv2.getWindowProperty('Frame', cv2.WND_PROP_VISIBLE) < 1:
-        break
-cam.release()
-cv2.destroyAllWindows()
+            faces = self.detector(gray)
+            for face in faces:
+                self.landmarks = self.predictor(gray, face)
+                self.draw_face_points()
+
+                self.left_eye = [(self.landmarks.part(36 + x).x, self.landmarks.part(36 + x).y) for x in range(0, 6)]
+                self.right_eye = [(self.landmarks.part(42 + x).x, self.landmarks.part(42 + x).y) for x in range(0, 6)]
+                eye_dec = EyeDetection(self.frame, self.drawing_frame, self.left_eye, self.right_eye).detect_eye()
+            cv2.imshow("Frame", self.drawing_frame)
+
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+            if cv2.getWindowProperty('Frame', cv2.WND_PROP_VISIBLE) < 1:
+                break
+        self.cam.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    fr_obj = FaceRecognition()
+    fr_obj.face_recognition()
